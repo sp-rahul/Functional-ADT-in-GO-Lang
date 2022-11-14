2db0de090c3bf0576717364f1cd34a099d1a8d74
sp-rahul <jksruhulamin123@gmail.com>
2022-11-14 11:27:14 +0600

    Second

--- Tree/tree/treeImplement.go
+++ Tree/tree/treeImplement.go
@@ -22,7 +22,7 @@ func set_right(key int, parent int, tree []int) []int {
 
 func getRightChild(tree []int, index int) int {
 	indexOfRightChild := (2 * index) + 2
-	if len(tree) != 0 && indexOfRightChild <= 9 && tree[indexOfRightChild] != 0 { //tree[indexOfRightChild]  = 0 => no node is assigned
+	if len(tree) != 0 && indexOfRightChild <= 99 && tree[indexOfRightChild] != 0 { //tree[indexOfRightChild]  = 0 => no node is assigned
 		return tree[indexOfRightChild]
 	}
 	return -1
@@ -30,7 +30,7 @@ func getRightChild(tree []int, index int) int {
 
 func getLeftChild(tree []int, index int) int {
 	indexOfLeftChild := (2 * index) + 1
-	if len(tree) != 0 && indexOfLeftChild <= 9 && tree[indexOfLeftChild] != 0 {
+	if len(tree) != 0 && indexOfLeftChild <= 99 && tree[indexOfLeftChild] != 0 {
 		return tree[indexOfLeftChild]
 	}
 	return -1
@@ -43,7 +43,7 @@ func traverseTree(actualTree []int, tree []int, indexOfNode int, indexAtActualTr
 		// indexOfNode = 0
 		indexAtActualTree++
 
-		if indexOfNode < 9 {
+		if indexOfNode < 99 {
 			val := tree[0]
 			printTree(actualTree, tree, val, indexAtActualTree)
 			traverseTree(actualTree, tree[1:], indexOfNode, indexAtActualTree)
@@ -59,11 +59,11 @@ func printTree(actualTree []int, tree []int, valAtIndex int, indexAtActualTree i
 		left := getLeftChild(actualTree, actualTree[indexAtActualTree])
 		right := getRightChild(actualTree, actualTree[indexAtActualTree])
 
-		if left != -1 && left < 10 { // -1 = no left child
+		if left != -1 && left < 100 { // -1 = no left child
 			fmt.Printf("Left child of %d is = ", actualTree[indexAtActualTree])
 			fmt.Println(left)
 		}
-		if right != -1 && left < 10 { // -1 = no right child
+		if right != -1 && left < 100 { // -1 = no right child
 			fmt.Printf("Right child of %d is = ", actualTree[indexAtActualTree])
 			fmt.Println(right)
 		}
@@ -73,7 +73,7 @@ func printTree(actualTree []int, tree []int, valAtIndex int, indexAtActualTree i
 
 func main() {
 
-	tree := make([]int, 10)
+	tree := make([]int, 100)
 
 	tree = root(tree, 1)
 	tree = set_left(2, 1, tree)
